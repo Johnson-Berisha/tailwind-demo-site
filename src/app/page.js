3681c9d9b10dd7c9bd527b4db53ec8b8cf34cbb3
Gjonson <gjonsonberisha@gmail.com>
2025-09-07 09:11:22 +0800

    more experiments

--- src/app/page.js
+++ src/app/page.js
@@ -3,16 +3,9 @@
 export default function Home() {
   return (
     <main>
-      <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
-        <div className="text-xl font-medium text-black dark:text-white">ChitChattt</div>
-        <p className="text-gray-500 dark:text-gray-400">Cool tailwind, very cool text and box.</p>
+      <div className="bg-violet-200 h-10 w-full border-2 border-violet-600 rounded-md my-4 p-6 flex justify-center items-center">
+      <h1 className="text-center text-black font-mono font-extrabold">hellooo</h1>
       </div>
-      <div class="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
-  <div>
-    <div class="text-xl font-medium text-black dark:text-white">ChitChat</div>
-    <p class="text-gray-500 dark:text-gray-400">You have a new message!</p>
-  </div>
-</div>
     </main>
   );
 }
